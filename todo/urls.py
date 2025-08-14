a2019909e6107dd11e908f23f87b51e1a6d28e9f
keshayadav5 <kesavyadav992@gmail.com>
2025-08-15 01:12:42 +0530

    added functionality of is_completed

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('addTask',views.addTask, name='addTask')
+  path('addTask',views.addTask, name='addTask'),
+  path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+  path('mark_as_not_done/<int:pk>/', views.mark_as_not_done, name='mark_as_not_done'),
+  path('edit_task/<int:pk>/',views.edit_task, name='edit_task')
 ]
